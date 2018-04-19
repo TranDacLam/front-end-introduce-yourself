cdf9e5125a95a8812b2859679a0b2815aca30185
tranlam <tdlam2105@gmail.com>
2018-04-19 17:21:02 +0700

    V4

--- js/main.js
+++ js/main.js
@@ -86,11 +86,13 @@
 			
 			event.preventDefault();
 			var aside = $('#fh5co-aside'),
-				 grid = $('#fh5co-image-grid, .aside-toggle'),
+				 grid = $('#fh5co-image-grid'),
 				 bio = $('#fh5co-bio'),
 				 imgBg = $('.image-bg');
 
+
 			if (aside.hasClass('show')) {
+				
 				
 				if ($(window).width() <= 480 ) {
 					TweenLite.to(aside, -1, { 
@@ -103,10 +105,21 @@
 						ease: Power1.easeNone 
 					});
 				}
+				
+				TweenLite.to(grid, -1, { css: { 
+						"-webkit-transform" : "translate3d(0%, 0px, 0px)", 
+						"-moz-transform" : "translate3d(0%, 0px, 0px)", 
+						"-ms-transform" : "translate3d(0%, 0px, 0px)", 
+						"-o-transform" : "translate3d(0%, 0px, 0px)", 
+						"transform" : "translate3d(0%, 0px, 0px)"
+					}, 
+					ease: Power1.easeNone
+				});
 
 				TweenLite.to(bio, 1, { opacity: 0, delay: 0.2, ease: Power1.easeNone});
 				TweenLite.to(imgBg, 1, { opacity: 0, delay: 0.2, ease: Power1.easeNone});
 				
+
 				aside.removeClass('show');	
 			} else {
 
@@ -114,6 +127,28 @@
 					left: '0%',
 					ease: Power1.easeNone 
 				});
+
+				if ($(window).width() <= 480 ) {
+					TweenLite.to(grid, -1, { css: { 
+							"-webkit-transform" : "translate3d(85%, 0px, 0px)", 
+							"-moz-transform" : "translate3d(85%, 0px, 0px)", 
+							"-ms-transform" : "translate3d(85%, 0px, 0px)", 
+							"-o-transform" : "translate3d(85%, 0px, 0px)", 
+							"transform" : "translate3d(85%, 0px, 0px)" 
+						}, 
+						ease: Power1.easeNone
+					})
+				} else {
+					TweenLite.to(grid, -1, { css: { 
+							"-webkit-transform" : "translate3d(32%, 0px, 0px)", 
+							"-moz-transform" : "translate3d(32%, 0px, 0px)", 
+							"-ms-transform" : "translate3d(32%, 0px, 0px)", 
+							"-o-transform" : "translate3d(32%, 0px, 0px)", 
+							"transform" : "translate3d(32%, 0px, 0px)" 
+						}, 
+						ease: Power1.easeNone
+					});
+				}
 
 				TweenLite.to(bio, 1, { opacity: 1, delay: 0.3, ease: Power1.easeNone});
 				TweenLite.to(imgBg, 1, { opacity: 1, delay: 0.6, ease: Power1.easeNone});
@@ -132,10 +167,99 @@
 
 	}
 
+	var toggleContactAside = function() {
+
+		$('.aside-contact-toggle').click(function(event){
+			
+			event.preventDefault();
+			var aside = $('#fh5co-contact-aside'),
+				 grid = $('#fh5co-image-grid'),
+				 bio = $('#fh5co-contact-bio'),
+				 imgBg = $('.image-bg');
+
+
+			if (aside.hasClass('show')) {
+				
+				
+				if ($(window).width() <= 480 ) {
+					TweenLite.to(aside, -1, { 
+						left: '-85%',
+						ease: Power1.easeNone 
+					});
+				} else {
+					TweenLite.to(aside, -1, { 
+						left: '-50%',
+						ease: Power1.easeNone 
+					});
+				}
+				
+				TweenLite.to(grid, -1, { css: { 
+						"-webkit-transform" : "translate3d(0%, 0px, 0px)", 
+						"-moz-transform" : "translate3d(0%, 0px, 0px)", 
+						"-ms-transform" : "translate3d(0%, 0px, 0px)", 
+						"-o-transform" : "translate3d(0%, 0px, 0px)", 
+						"transform" : "translate3d(0%, 0px, 0px)"
+					}, 
+					ease: Power1.easeNone
+				});
+
+				TweenLite.to(bio, 1, { opacity: 0, delay: 0.2, ease: Power1.easeNone});
+				TweenLite.to(imgBg, 1, { opacity: 0, delay: 0.2, ease: Power1.easeNone});
+				
+
+				aside.removeClass('show');	
+			} else {
+
+				TweenLite.to(aside, -1, { 
+					left: '0%',
+					ease: Power1.easeNone 
+				});
+
+				if ($(window).width() <= 480 ) {
+					TweenLite.to(grid, -1, { css: { 
+							"-webkit-transform" : "translate3d(85%, 0px, 0px)", 
+							"-moz-transform" : "translate3d(85%, 0px, 0px)", 
+							"-ms-transform" : "translate3d(85%, 0px, 0px)", 
+							"-o-transform" : "translate3d(85%, 0px, 0px)", 
+							"transform" : "translate3d(85%, 0px, 0px)" 
+						}, 
+						ease: Power1.easeNone
+					})
+				} else {
+					TweenLite.to(grid, -1, { css: { 
+							"-webkit-transform" : "translate3d(32%, 0px, 0px)", 
+							"-moz-transform" : "translate3d(32%, 0px, 0px)", 
+							"-ms-transform" : "translate3d(32%, 0px, 0px)", 
+							"-o-transform" : "translate3d(32%, 0px, 0px)", 
+							"transform" : "translate3d(32%, 0px, 0px)" 
+						}, 
+						ease: Power1.easeNone
+					});
+				}
+
+				TweenLite.to(bio, 1, { opacity: 1, delay: 0.3, ease: Power1.easeNone});
+				TweenLite.to(imgBg, 1, { opacity: 1, delay: 0.6, ease: Power1.easeNone});
+				aside.addClass('show');	
+			}
+		});
+
+		$(document).click(function (e) {
+	    var container = $(".aside-contact-toggle, #fh5co-contact-aside");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('#fh5co-contact-aside').hasClass('show') ) {
+	      	container.trigger('click');
+	      }
+	    }
+		});
+
+	}
+
+
 	$(function(){
 		contentWayPoint();
 		isotopeImageLoaded();
 		toggleAside();
+		toggleContactAside();
 	});
 
 	//Check to see if the window is top if not then display button
